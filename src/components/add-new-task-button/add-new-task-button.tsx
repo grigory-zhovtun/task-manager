ff7bc498a713fd6c8cc4cc2c9819d0ed5e18b4ea
grigory-zhovtun <grigory.zhovtun@gmail.com>
2022-05-25 08:30:10 +0300

    some changes

---
+++ src/components/add-new-task-button/add-new-task-button.tsx
@@ -0,0 +1,12 @@
+import './add-new-task-button.css'
+
+export const AddTaskButton = () => {
+    return (
+        <div className="AddTaskButton">
+            <svg width="27" height="19" viewBox="0 0 27 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <line x1="26" y1="1" x2="26" y2="13" stroke="#A5A5A5" stroke-width="2" stroke-linecap="round" />
+                <path d="M0 13L10 18.7735L10 7.2265L0 13ZM26 12L9 12L9 14L26 14L26 12Z" fill="#A5A5A5" />
+            </svg>
+        </div>
+    )
+}
--- src/components/todo-list/todo-list.tsx
+++ src/components/todo-list/todo-list.tsx
@@ -1,16 +1,27 @@
 import './todo-list.css'
 import bg from './bg.png'
-import {Input} from "../input/input";
+import { Input } from "../input/input";
+import { AddTaskButton } from '../add-new-task-button/add-new-task-button';
+import { ListItem } from '../list-item/list-item';
 
 type PropsType = {
     title: string
 }
 
-export const TodoList = ({title}: PropsType) => {
+export const TodoList = ({ title }: PropsType) => {
     return (
         <div className="TodoList">
             <span className="TodoListTitle">{title}</span>
-            <Input/>
+            <div className="AddTasksWrapper">
+                <Input />
+                <AddTaskButton />
+            </div>
+            <div className="ListItemsWrapper">
+                <ListItem/>
+                <ListItem/>
+                <ListItem/>
+            </div>
+            
         </div>
     )
 }
